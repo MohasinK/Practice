5b1baae530d57f7593c5c1dab68f0adaf9c1c9fc
unknown <mtamboli07@gmail.com>
2025-01-03 19:33:08 +0530

    3-1-25

---
+++ Practice/src/Practice/test.java
@@ -0,0 +1,29 @@
+package Practice;
+
+
+public class test {
+	
+	public static void main(String args[])
+	{
+		String name = "Harry Potter";
+		
+		System.out.println(name);
+		System.out.println("my name is : " + name);
+		
+		test t =  new test();
+		t.add();
+	}
+	
+	public void add()
+	{
+		int a=10;
+		
+		int b=20;
+		
+		int c=a+b;
+		
+		System.out.println("Addition of a and b is : "+c);
+	}
+	
+
+}
